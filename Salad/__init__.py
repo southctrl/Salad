9e73a21ed88fa6d772d5b7ff040aa9b2f356e48d
AV <134554554+southctrl@users.noreply.github.com>
2025-10-02 17:54:50 -0400

    Update __init__.py

--- Salad/__init__.py
+++ Salad/__init__.py
@@ -1,9 +1,11 @@
+__title__ = 'Salad'
+__author__ = 'ToddyTheNoobDud'
+__version__ = '1.0.0'
+__all__ = ['Rest', 'Node', 'Salad', 'Player', 'Queue', 'Track']
+
 from .Rest import Rest
 from .Node import Node
 from .Salad import Salad
 from .Player import Player
 from .Queue import Queue
 from .Track import Track
-
-__version__ = '1.0.0'
-__all__ = ['Rest', 'Node', 'Salad', 'Player', 'Queue', 'Track']
